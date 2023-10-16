88895f454fcf375f98ed76008a00879ea65d9870
02MARIII <MARISHHHALLL@gmail.com>
2023-10-16 18:52:39 +0100

    fix this one

--- _printf.c
+++ _printf.c
@@ -65,7 +65,8 @@ int _printf(const char *format, ...)
 	{
 		return (-1);
 	}
-
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	count = format_and_count(format, args);
 	va_end(args);
 	return (count);
